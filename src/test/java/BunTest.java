a5a192ec7ea5307db923aaccf29b928ddcf038ae
Daria Glushkova <speakintongues@yandex.com>
2023-03-31 18:33:28 +0300

    changed BunTest to not parameterized

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,41 +1,24 @@
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import praktikum.Bun;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(Parameterized.class)
 public class BunTest {
-    private final String bunName;
-    private final float bunPrice;
+    private final static String BUN_NAME = "Сладкая булочка";
+    private final static float BUN_PRICE = 150.5F;
     private Bun bun;
-    public BunTest(String bunName, float bunPrice) {
-        this.bunName = bunName;
-        this.bunPrice = bunPrice;
-    }
-    @Parameterized.Parameters(name = "Test data: Название булочки {0}, цена: {1}")
-    public static Object[][] getData() {
-        return new Object[][]{
-                {"Вкусная булка", 50},
-                {"Luchshaja bulochka", -263},
-                {"Interest булочка", 25.5F},
-                {"", 0},
-                {null, 1},
-        };
-    }
     @Before
     public void setUp() {
-        bun = new Bun(bunName, bunPrice);
+        bun = new Bun(BUN_NAME, BUN_PRICE);
     }
     @Test
     public void getBunName(){
-        assertEquals("Названия отличаются", bunName, bun.getName());
+        assertEquals("Названия отличаются", BUN_NAME, bun.getName());
     }
     @Test
     public void getBunPrice(){
-        assertEquals("Цены отличаются", bunPrice, bun.getPrice(), 0);
+        assertEquals("Цены отличаются", BUN_PRICE, bun.getPrice(), 0);
     }
 
 }
