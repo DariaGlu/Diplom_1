b0949b4a9be842e5eb93816eac62e01d74b1da3e
Daria Glushkova <speakintongues@yandex.com>
2023-03-31 19:36:18 +0300

    BunTest: did reformat code added 2 test classes IngredientTest and parameterized IngredientTypeTest

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -8,16 +8,19 @@ public class BunTest {
     private final static String BUN_NAME = "Сладкая булочка";
     private final static float BUN_PRICE = 150.5F;
     private Bun bun;
+
     @Before
     public void setUp() {
         bun = new Bun(BUN_NAME, BUN_PRICE);
     }
+
     @Test
-    public void getBunName(){
+    public void getBunName() {
         assertEquals("Названия отличаются", BUN_NAME, bun.getName());
     }
+
     @Test
-    public void getBunPrice(){
+    public void getBunPrice() {
         assertEquals("Цены отличаются", BUN_PRICE, bun.getPrice(), 0);
     }
 
---
+++ src/test/java/IngredientTypeTest.java
@@ -0,0 +1,40 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
+
+import static org.junit.Assert.assertEquals;
+import static praktikum.IngredientType.FILLING;
+import static praktikum.IngredientType.SAUCE;
+
+@RunWith(Parameterized.class)
+public class IngredientTypeTest {
+    private final static String INGREDIENT_NAME = "Что-то вкусненькое";
+    private final static float INGREDIENT_PRICE = 99.99F;
+    private final IngredientType ingredientType;
+    private Ingredient ingredient;
+
+    public IngredientTypeTest(IngredientType ingredientType) {
+        this.ingredientType = ingredientType;
+    }
+
+    @Parameterized.Parameters(name = "Test data: Type of an ingredient: {0}")
+    public static Object[][] getData() {
+        return new Object[][]{
+                {FILLING},
+                {SAUCE},
+        };
+    }
+
+    @Before
+    public void setUp() {
+        ingredient = new Ingredient(ingredientType, INGREDIENT_NAME, INGREDIENT_PRICE);
+    }
+
+    @Test
+    public void getIngredientType() {
+        assertEquals("Типы начинок отличаются", ingredientType, ingredient.getType());
+    }
+}
