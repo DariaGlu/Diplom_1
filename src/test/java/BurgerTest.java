e8de713893cb0d8180a550229629414b5ec816b0
Daria Glushkova <speakintongues@yandex.com>
2023-04-01 14:08:03 +0300

    covered all methods of class Burger with tests + made some changes

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,29 +8,36 @@ import org.mockito.junit.MockitoJUnitRunner;
 import praktikum.Bun;
 import praktikum.Burger;
 import praktikum.Ingredient;
+import praktikum.IngredientType;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static praktikum.IngredientType.FILLING;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BurgerTest {
-    private final List<Ingredient> ingredients = new ArrayList<>();
+    private final static String INGREDIENT_NAME = "2 мясных котлеты гриль";
+    private final static String BUN_NAME = "всё на булочке с кнужутом";
+    private final static float INGREDIENT_PRICE = 10.5F;
+    private final static float BUN_PRICE = 7.5F;
+    private final static IngredientType INGREDIENT_TYPE = FILLING;
     @Mock
     Bun bun;
     @Mock
-    Ingredient ingredient;
+    Ingredient ingredient1;
     @Mock
-    Ingredient ingredientSauce;
-    @Mock
-    Ingredient ingredientFilling;
+    Ingredient ingredient2;
     private Burger burger;
 
     @Before
     public void setUp() {
         burger = new Burger();
+        Mockito.when(ingredient1.getName()).thenReturn(INGREDIENT_NAME);
+        Mockito.when(bun.getName()).thenReturn(BUN_NAME);
+        Mockito.when(ingredient1.getPrice()).thenReturn(INGREDIENT_PRICE);
+        Mockito.when(bun.getPrice()).thenReturn(BUN_PRICE);
+        Mockito.when(ingredient1.getType()).thenReturn(INGREDIENT_TYPE);
     }
 
     @Test
@@ -41,25 +49,45 @@ public class BurgerTest {
     @Test
     public void addIngredientCheck() {
         int ListBeforeAdding = burger.ingredients.size();
-        burger.addIngredient(ingredient);
+        burger.addIngredient(ingredient1);
         assertTrue("Список ингредиентов не увеличился", burger.ingredients.size() > ListBeforeAdding);
     }
 
     @Test
     public void removeIngredientCheck() {
-        burger.addIngredient(ingredient);
+        burger.addIngredient(ingredient1);
         int ListBeforeRemove = burger.ingredients.size();
         burger.removeIngredient(0);
         assertTrue("Список ингредиентов не уменьшился", ListBeforeRemove > burger.ingredients.size());
     }
+
     @Test
     public void moveIngredientCheck() {
-        String expectedFillingName = "две мясных котлеты гриль";
-        burger.addIngredient(ingredient);
-        burger.addIngredient(ingredientSauce);
-        burger.addIngredient(ingredientFilling);
-        Mockito.when(ingredientFilling.getName()).thenReturn(expectedFillingName);
-        burger.moveIngredient(2,0);
-        assertEquals("Ингредиент не переместился", ingredientFilling.getName(), burger.ingredients.get(0).getName());
+        int oldIndex = 1;
+        int newIndex = 0;
+        burger.addIngredient(ingredient1);
+        burger.addIngredient(ingredient2);
+        burger.moveIngredient(oldIndex, newIndex);
+        assertEquals("Ингредиент не переместился", newIndex, burger.ingredients.indexOf(ingredient2));
     }
+
+    @Test
+    public void burgerGetPrice() {
+        float totalPrice = BUN_PRICE * 2 + INGREDIENT_PRICE;
+        burger.setBuns(bun);
+        burger.addIngredient(ingredient1);
+        Assert.assertThat("Цена отличается", burger.getPrice(), is(totalPrice));
+    }
+
+    @Test
+    public void burgerGetReceipt() {
+        burger.setBuns(bun);
+        burger.addIngredient(ingredient1);
+        String totalReceipt = String.format("(==== %s ====)%n", BUN_NAME) +
+                String.format("= %s %s =%n", INGREDIENT_TYPE.toString().toLowerCase(), INGREDIENT_NAME) +
+                String.format("(==== %s ====)%n", BUN_NAME) +
+                String.format("%nPrice: %f%n", burger.getPrice());
+        assertEquals("Чеки отличаются", totalReceipt, burger.getReceipt());
+    }
+
 }
